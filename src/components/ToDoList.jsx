4f847baf54bd0467f8d06ee0398f39d3dba84dbd
jss118 <joesmith_guitar@hotmail.co.uk>
2022-11-07 16:34:31 +0000

    add to-do-list functionality

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState } from "react";
 
 const ToDoList = () => {
-  return <div>TODOLIST!!!</div>;
+  const [listItems, setListItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const handleChange = event => {
+    setNewItem(event.target.value);
+  };
+
+  const handleClick = event => {
+    event.preventDefault();
+    setListItems(items => {
+      const newItems = [newItem, ...items];
+      return newItems;
+    });
+    setNewItem("");
+  };
+
+  const removeItem = event => {
+    const deleteItem = event.target.value;
+    setListItems(currentList => {
+      const newList = currentList.filter(item => item !== deleteItem);
+      return newList;
+    });
+  };
+
+  return (
+    <section>
+      <input onChange={handleChange} type="text" />
+      <button onClick={handleClick}>Add to list</button>
+      <ul>
+        {listItems.map(item => {
+          return (
+            <li>
+              {item}
+              <button value={item} onClick={removeItem}>
+                x
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 
 export default ToDoList;
