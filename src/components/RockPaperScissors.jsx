19a11822a9e7fefc87b5c8bed3f0dfbf9961b2b2
jss118 <joesmith_guitar@hotmail.co.uk>
2022-11-09 14:56:56 +0000

    make a start to RPS game

--- src/components/RockPaperScissors.jsx
+++ src/components/RockPaperScissors.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState } from "react";
 
 const RockPaperScissors = () => {
-  return <div>ROCK PAPER SCISSORS!</div>;
+  const [startGame, setStartGame] = useState(false);
+  const [scores, setScores] = useState({ player: 0, computer: 0 });
+
+  const start = () => {
+    setStartGame(true);
+  };
+
+  return (
+    <section className="RPS__section">
+      <h1>Rock Paper Scissors</h1>
+      {startGame ? (
+        <div className="game">
+          <div className="game_scores">
+            <div className="score">
+              <h3>Player:</h3>
+              <h4>{scores.player}</h4>
+            </div>
+            <div className="score">
+              <h3>Computer:</h3>
+              <h4>{scores.computer}</h4>
+            </div>
+          </div>
+          <div className="options">
+            <button>Rock</button>
+            <button>Paper</button>
+            <button>Scissors</button>
+          </div>
+        </div>
+      ) : (
+        <button onClick={start} className="RPS__start-btn">
+          Start
+        </button>
+      )}
+    </section>
+  );
 };
 
 export default RockPaperScissors;
