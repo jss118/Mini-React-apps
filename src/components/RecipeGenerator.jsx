3211277c10cccf582d11a53c8bd5a3e8724bead1
jss118 <joesmith_guitar@hotmail.co.uk>
2023-01-09 17:27:42 +0000

    add first recipe API

--- src/components/RecipeGenerator.jsx
+++ src/components/RecipeGenerator.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://tasty.p.rapidapi.com/recipes/list",
+  params: { from: "", size: "", tags: "" },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+  },
+};
+
+Axios.request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
 const RecipeGenerator = () => {
-  return <div>Recipe generator goes here</div>;
+  const [recipeList, setRecipeList] = useState("");
+
+  return (
+    <div className="recipe_div">
+      <button className="recipe_gen-btn">Generate Recipe!</button>
+    </div>
+  );
 };
 
 export default RecipeGenerator;
