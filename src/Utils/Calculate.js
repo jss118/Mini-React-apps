941579ed9b06e751662bf05da4927da67039cece
jss118 <joesmith_guitar@hotmail.co.uk>
2023-02-03 10:15:51 +0000

    small refacter for the negative numbers part of the calculator

--- src/Utils/Calculate.js
+++ src/Utils/Calculate.js
@@ -1,31 +1,25 @@
 const calculate = sumStr => {
-  let negativeNum = sumStr[0];
-
-  if (sumStr[0] === "-") {
-    for (let i = 1; i < sumStr.length; i++) {
-      negativeNum += sumStr[i];
-      if (/[+\-x/]/.test(sumStr[i + 1])) {
-        break;
-      }
-    }
-  }
+  //seperate numbers and operaters into seperate arrays
   let numbers = sumStr.match(/(\d+)/g);
   let operaters = sumStr.match(/[+\-x/]/g);
 
   let sumArr = [];
-  if (sumStr[0] === "-") {
+
+  //if there is a negative number, there will be an extra operator making it's length equal to the numbers length. If this is true, remove the minus from operaters, and replace the first number with the negative number.
+  if (numbers.length === operaters.length) {
     operaters.shift();
-    numbers.shift();
-    numbers.unshift(negativeNum);
+    numbers.splice(0, 1, -Math.abs(numbers[0]));
   }
 
+  //push the numbers and operaters into the sumArr
   for (let i = 0; i < numbers.length; i++) {
     sumArr.push(numbers[i]);
+    //because the numbers array has one extra value, use conditional logic so that an undefined value does not get pushed into the sumArr
     if (operaters[i] !== undefined) {
       sumArr.push(operaters[i]);
     }
   }
-
+  // Multiplication and division first, then additions and subtractions
   for (let j = 0; j < operaters.length; j++) {
     for (let i = 0; i < sumArr.length; i++) {
       if (sumArr.includes("+") || sumArr.includes("-")) {
@@ -35,6 +29,7 @@ const calculate = sumStr => {
             3,
             String(Number(sumArr[i - 1]) * Number(sumArr[i + 1]))
           );
+          //3+3x2 now equals 3+6, the '3x2' is replaced by the answer
         }
         if (sumArr[i] === "/") {
           sumArr.splice(
@@ -46,6 +41,8 @@ const calculate = sumStr => {
       }
     }
   }
+
+  //convert the stringed numbers into numbers whilst updating the answer variable
 
   let answer = Number(sumArr[0]);
   sumArr = sumArr.slice(1);
@@ -62,6 +59,7 @@ const calculate = sumStr => {
     }
   }
 
+  //return the answer as a string
   return String(answer);
 };
 
